f78b0bac6c691ba49d654fed555785d68aa464fe
LetMeL1ve <roman.null13@gmail.com>
2025-08-26 17:20:23 +0300

    Created setup function

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <filesystem>
+#include <string>
+#include <fstream>
+
+float course;
+std::string currency1, currency2;
+const std::string path = std::filesystem::current_path().string() + "\\data.txt";
+
+void setup();
 
 int main() {
-    std::cout << "Hi Converter." << std::endl;
+    setup();
+    std::cout << currency1 << " - " << course << " - " << currency2;
     return 0;
+}
+
+void setup() {
+    std::ifstream fin;
+    fin.open(path);
+
+    std::string line;
+    getline(fin, line); // Getting data
+
+    currency1 = line.substr(0, 3);
+    course = std::stof(line.substr(4, 8));
+    currency2 = line.substr(9, 12);
+
+    fin.close(); // Closing the ifstream
 }
