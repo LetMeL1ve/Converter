6b8297f7e0fea4c4015505fefc0f9617f4554289
LetMeL1ve <roman.null13@gmail.com>
2025-08-28 16:58:03 +0300

    Added change_currency mathod.

--- src/main.cpp
+++ src/main.cpp
@@ -17,21 +17,31 @@ void change_currency();
 
 void convert();
 
+void rewrite_file();
+
+std::string get_currency_from_user();
+
+bool contains_only_letters(std::string s);
+
 int main() {
     setup();
     std::cout << "1 " << currency1 << " = " << course << " " << currency2 << '\n';
     change_course();
+    change_currency();
     std::cout << "1 " << currency1 << " = " << course << " " << currency2 << '\n';
     getchar();
     return 0;
 }
 
+void rewrite_file() {
+    std::ofstream fout(path);
+    fout << currency1 << delimeter << course << delimeter << currency2;
+    fout.close();
+}
+
 void setup() {
     if (!std::filesystem::exists(path)) {
-        std::ofstream fout;
-        fout.open(path);
-        fout << currency1 << ' ' << course << ' ' << currency2;
-        fout.close();
+        rewrite_file();
     }
 
     std::ifstream fin;
@@ -58,7 +68,32 @@ void change_course() {
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
     }
-    std::ofstream fout(path);
-    fout << currency1 << delimeter << course << delimeter << currency2;
-    fout.close();
+    rewrite_file();
+}
+
+bool contains_only_letters(std::string s) {
+    for (char ch : s) {
+        if (!(64 < toupper(ch) && toupper(ch) < 91))
+            return false;
+    }
+    return true;
+}
+
+std::string get_currency_from_user() {
+    std::string currency;
+    while (true) {
+        std::cout << "Enter new currency: ";
+        if (std::cin >> currency && contains_only_letters(currency))
+            return currency.substr(0, 3);
+        else {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+    }
+}
+
+void change_currency() {
+    currency1 = get_currency_from_user();
+    currency2 = get_currency_from_user();
+    rewrite_file();
 }
