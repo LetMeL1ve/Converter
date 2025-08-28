03fc763ab7b22cc9cdb888f912830b65a7106024
LetMeL1ve <roman.null13@gmail.com>
2025-08-28 12:09:46 +0300

    Deleted change_course function and fixed setup

--- src/main.cpp
+++ src/main.cpp
@@ -2,61 +2,46 @@
 #include <filesystem>
 #include <string>
 #include <fstream>
+#include <sstream>
 
-float course;
-std::string currency1, currency2;
+float course = 1.0f;
+std::string currency1 = "NG", currency2 = "NG";
 const std::string path = std::filesystem::current_path().string() + "\\data.txt";
-const char delimeter = '/';
+const char delimeter = ' ';
 
 void setup();
 
 void change_course();
 
+void change_currency();
+
+void convert();
+
 int main() {
     setup();
-    change_course();
+    std::cout << "1 " << currency1 << " = " << course << " " << currency2;
+    getchar();
     return 0;
 }
 
 void setup() {
+    if (!std::filesystem::exists(path)) {
+        std::ofstream fout;
+        fout.open(path);
+        fout << currency1 << ' ' << course << ' ' << currency2;
+        fout.close();
+    }
+
     std::ifstream fin;
     fin.open(path);
-    if (!fin.is_open()) 
+    if (!fin.is_open()) {
         std::cerr << "Unable to open the file.";
-
-    std::string temp;
-
-    getline(fin, temp, delimeter);
-    currency1 = temp;
-
-    getline(fin, temp, delimeter);
-    course = std::stof(temp);
-    
-    getline(fin, temp, delimeter);
-    currency2 = temp;
-
-    fin.close(); // Closing the ifstream
-}
-
-void change_course() {
-    std::cout << "Current course: " << course << ".\nEnter new course:";
-    float new_course = 0;
-
-    // Getting new course
-    while (true) {
-        if (std::cin >> new_course)
-            break;
-        else {
-            std::cout << "Please try again.";
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        }
+        return;
     }
-    course = new_course;
 
-    // Saving new course to the file
-    std::ofstream fout;
-    fout.open(path);
-    fout << currency1 << delimeter << course << delimeter << currency2;
-    fout.close();
+    fin >> currency1;
+    fin >> course;
+    fin >> currency2;
+    
+    fin.close(); // Closing the ifstream
 }
